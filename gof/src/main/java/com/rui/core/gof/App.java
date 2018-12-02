a984a96b297fe6526c2b0729bafd3ddf82434fb7
胡锐 <hurui_h@qq.com>
2018-12-02 16:37:05 +0800

    添加获取单例的测试方法

--- gof/src/main/java/com/rui/core/gof/App.java
+++ gof/src/main/java/com/rui/core/gof/App.java
@@ -1,5 +1,8 @@
 package com.rui.core.gof;
 
+import com.rui.core.gof.singdemo.SingDemo;
+import com.rui.core.gof.singdemo.SingDemoB;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SingDemo singDemo = SingDemo.getIntance();
+        SingDemo singDemo2 = SingDemo.getIntance();
+        System.out.println(singDemo);
+        System.out.println(singDemo2);
+        
+        SingDemoB singDemoB = SingDemoB.getIntance();
+        SingDemoB singDemoB2 = SingDemoB.getIntance();
+        System.out.println(singDemoB);
+        System.out.println(singDemoB2);
+        
+        
     }
 }
